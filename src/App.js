f3d1381257da16aced4d42488417205e093c20b8
doguozcan <76822280+doguozcan@users.noreply.github.com>
2024-01-10 20:27:57 +0300

    Some changes in router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import VideoGame from "./pages/videogame";
 import Movie from "./pages/movie";
 import Kickstarter from "./pages/kickstarter";
@@ -10,12 +10,12 @@ function App() {
     <Router basename="/visualize-data-with-a-treemap-diagram">
       <div>
         <Header />
-        <Routes>
-          <Route exact path="/" element={<VideoGame />} />
-          <Route path="/videogame" element={<VideoGame />} />
-          <Route path="/movie" element={<Movie />} />
-          <Route path="/kickstarter" element={<Kickstarter />} />
-        </Routes>
+        <Switch>
+          <Route exact path="/" component={VideoGame} />
+          <Route path="/videogame" component={VideoGame} />
+          <Route path="/movie" component={Movie} />
+          <Route path="/kickstarter" component={Kickstarter} />
+        </Switch>
       </div>
     </Router>
   );
